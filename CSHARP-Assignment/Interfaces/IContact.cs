1a6f3de40c69734c14dbaaa65aff0b69490d42ae
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2023-01-16 09:38:46 +0100

    First commit  on console app

---
+++ CSHARP-Assignment/Interfaces/IContact.cs
@@ -0,0 +1,15 @@
+﻿namespace CSHARP_Assignment.Interfaces;
+
+internal interface IContact
+{
+    Guid Id { get; set; };
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    string Email { get; set; }      
+    string Phone { get; set; }
+    string Address { get; set; }
+    string ZipCode { get; set; }
+    string City { get; set; }
+
+}
+}
