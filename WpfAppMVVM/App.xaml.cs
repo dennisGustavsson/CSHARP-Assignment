f75fba82db0a2f664e2aaa4f595b61a12defc0ee
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2023-01-19 17:45:00 +0100

    Working on the views for wpf app

--- WpfAppMVVM/App.xaml.cs
+++ WpfAppMVVM/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WpfAppMVVM.MVVM.ViewModels;
 
 namespace WpfAppMVVM
 {
@@ -13,5 +14,14 @@ namespace WpfAppMVVM
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
--- WpfAppMVVM/MVVM/ViewModels/MainViewModel.cs
+++ WpfAppMVVM/MVVM/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,27 @@ using System.Threading.Tasks;
 
 namespace WpfAppMVVM.MVVM.ViewModels;
 
-public class MainViewModel : ObservableObject
+public partial class MainViewModel : ObservableObject
 
 {
-    
+    [ObservableProperty]
+    private ObservableObject currentViewModel;
+
+    [RelayCommand]
+    private void AddContact()
+    {
+        CurrentViewModel = new AddContactViewModel();
+    }
+
+    [RelayCommand]
+    private void Contacts() 
+    {
+        CurrentViewModel = new ContactsViewModel();
+    }
+
+
+    public MainViewModel()
+    {
+        CurrentViewModel = new ContactsViewModel();
+    }
 }
